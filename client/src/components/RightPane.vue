0aa65aa514bbabd25d9adf805d877b26f2a696c9
mhairimccrindle@gmail.com <mhairimccrindle@gmail.com>
2019-09-09 19:55:24 +0100

    tabs added

--- client/src/components/RightPane.vue
+++ client/src/components/RightPane.vue
@@ -1,11 +1,24 @@
 <template lang="html">
 <div id="rightPane">
-<button @click="tab = 'details'">Details</button>
-<button @click="tab = 'favourites'">Favourites</button>
-<button @click="tab = 'recommendations'">Nearby</button>
-<details-view  :favourites="favourites" v-if="chosenMural && tab === 'details'" :chosenMural="chosenMural" />
-<favourites-view :favourites="favourites" v-if="tab === 'favourites'" />
-<recommendations-view :recommendations="recommendations" v-if="tab === 'recommendations'"/>
+  <div class="tabs">
+    <input type="radio" name="tabs" id="tabone" checked="checked" @click="tab = 'details'">
+    <label for="tabone">Details</label>
+    <div class="tab">
+      <details-view  :favourites="favourites" v-if="chosenMural && tab === 'details'" :chosenMural="chosenMural" />
+    </div>
+
+    <input type="radio" name="tabs" id="tabtwo" @click="tab = 'favourites'">
+    <label for="tabtwo">Favourites</label>
+    <div class="tab">
+      <favourites-view :favourites="favourites" v-if="tab === 'favourites'" />
+    </div>
+
+    <input type="radio" name="tabs" id="tabthree" @click="tab = 'recommendations'">
+    <label for="tabthree">Nearby</label>
+    <div class="tab">
+      <recommendations-view :recommendations="recommendations" v-if="tab === 'recommendations'"/>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -17,6 +30,7 @@ export default {
   name: 'right-pane',
   data() {
     return {
+      isActive: true,
       tab: 'details'
     }
   },
@@ -35,8 +49,67 @@ export default {
 	height:80vh;
 }
 
-#tabtop {
-  background-color: grey;
-  height: 4vh;
+.tabs {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.tabs label {
+  order: 1;
+  display: block;
+  padding: 1rem 2rem;
+  margin-right: 0.2rem;
+  cursor: pointer;
+  background:  slategrey;
+  color: snow;
+  font-weight: bold;
+  transition: ease 0.2s;
+}
+
+.tabs .tab {
+  order: 99;
+  flex-grow: 1;
+  width: 100%;
+  display: none;
+  padding: 1rem;
+  background: #fff;
+}
+
+.tabs input[type="radio"] {
+  display: none;
+}
+
+.tabs input[type="radio"]:checked + label {
+  background: #fff;
+  color: black;
+  border: 1px solid black;
+}
+
+.tabs input[type="radio"]:checked + label + .tab {
+  display: block;
+}
+
+@media (max-width: 45em) {
+  .tabs .tab,
+  .tabs label {
+    order: initial;
+  }
+
+  .tabs label {
+    width: 100%;
+    margin-right: 0;
+    margin-top: 0.2rem;
+  }
+}
+
+body {
+  min-height: 100vh;
+  box-sizing: border-box;
+  padding-top: 10vh;
+  font-weight: 300;
+  line-height: 1.5;
+  max-width: 60rem;
+  margin: 0 auto;
+  font-size: 112%;
 }
 </style>
